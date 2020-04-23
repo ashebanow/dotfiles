b01746b06c63e20306b0e663cc1adb2a9c085ef7
Andrew Shebanow <ashebanow@gmail.com>
2020-04-23 15:45:35 -0700

    add brew zsh-completions and fix .dotfiles location

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,7 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+# add homebrew completions to fpath
+if type brew &>/dev/null; then
+	fpath=($(brew --prefix)/share/zsh-completions $fpath)
+fi
