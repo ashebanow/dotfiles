f2d9c89248582c4417ec85685e9ba8d461835efb
Andrew Shebanow <ashebanow@gmail.com>
2025-05-08 16:26:17 -0700

    fix nix initialization in fish - now tests for file existense before sourcing

--- home/private_dot_config/private_fish/conf.d/002_nix.fish
+++ home/private_dot_config/private_fish/conf.d/002_nix.fish
@@ -1,2 +1,6 @@
 #fish_add_path -g "~/.nix-profile/bin"
-source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+
+set NIX_PROFILE_FILE="/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
+if test -f "${NIX_PROFILE_FILE}"
+  source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+end
