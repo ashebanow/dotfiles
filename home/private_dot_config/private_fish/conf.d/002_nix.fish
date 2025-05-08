1b9a099330bd27c7efc409cdd1b9e9131a3621cf
Andrew Shebanow <ashebanow@gmail.com>
2025-05-08 16:27:48 -0700

    oops, used zsh syntax for fish variable

--- home/private_dot_config/private_fish/conf.d/002_nix.fish
+++ home/private_dot_config/private_fish/conf.d/002_nix.fish
@@ -1,6 +1,6 @@
 #fish_add_path -g "~/.nix-profile/bin"
 
-set NIX_PROFILE_FILE="/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
-if test -f "${NIX_PROFILE_FILE}"
+set NIX_PROFILE_FILE "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish"
+if test -f "$NIX_PROFILE_FILE"
   source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
 end
