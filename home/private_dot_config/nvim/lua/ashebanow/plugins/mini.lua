2a85d3f666ad4b9ab16753dec1267ace0141c4c7
Andrew Shebanow <ashebanow@gmail.com>
2025-06-08 10:51:33 -0700

    nvim: add mini.surround, remove flash

--- home/private_dot_config/nvim/lua/ashebanow/plugins/mini.lua
+++ home/private_dot_config/nvim/lua/ashebanow/plugins/mini.lua
@@ -1,10 +1,17 @@
 return {
-  {
-    "echasnovski/mini.ai",
-    opts = {},
-  },
-  {
-    "echasnovski/mini.bracketed",
-    opts = {},
-  },
+    {
+        "echasnovski/mini.ai",
+        opts = {},
+    },
+    {
+        "echasnovski/mini.bracketed",
+        opts = {},
+    },
+    {
+        "echasnovski/mini.surround",
+        config = function()
+            require("mini.surround").setup()
+        end,
+        opts = {},
+    },
 }
