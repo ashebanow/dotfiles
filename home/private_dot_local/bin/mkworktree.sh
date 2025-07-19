738637a3956e7bb48f774f6cac194c7e531ea615
Andrew Shebanow <ashebanow@gmail.com>
2025-07-19 16:27:45 -0700

    add simple mkworktree.sh script

---
+++ home/private_dot_local/bin/mkworktree.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+# first arg is the name of the project
+PROJECT=$1
+
+# create a new directory for the repository
+mkdir "${PROJECT}" && cd "${PROJECT}"
+# glone the repository (bare) and hide it in a hidden direcotry
+git clone --bare git@github.com:ashebanow/"${PROJECT}".git .bare
+# create a `.git` file that points to the bare repo
+echo "gitdir: ./.bare" > .git
+
+git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+git fetch
+git for-each-ref --format='%(refname:short)' refs/heads | xargs -n1 -I{} git branch --set-upstream-to=origin/{}
+
+git worktree add main
